d7887f6f4825dca33503260a49fb620844b1d140
zhang.jian <zhang.jian@chinaebi.com>
2017-09-12 11:17:10 +0800

    [fix]

---
+++ html/update.php
@@ -0,0 +1,19 @@
+<?php
+header("content-type:text/html;charset=utf-8");
+$conn=@mysql_connect("192.168.20.220","prodba","12wsxCDE#");
+
+if(!$conn){
+        echo "<h2>错误编码:".mysql_errno()."</h2>";
+        echo "<h2>错误编码:".mysql_error()."</h2>";
+}else{
+
+        mysql_select_db("test");
+        mysql_query("set names utf8");
+
+        $mtime=explode(' ',microtime());
+        $startTime=$mtime[1]+$mtime[0];
+
+        $sql = "insert into t3(balance,name1,name2,name3) values ('$startTime','$startTime','$startTime','$startTime')";
+        mysql_query($sql);
+        mysql_close($conn);
+}
